2f55609f1dc7ddb9be8a4533e4e2aacfdd875a73
Chris <chris.dudziak@gmail.com>
2024-04-01 16:51:14 -0400

    Update StartTriggers.ps1

--- .github/workflows/StartTriggers.ps1
+++ .github/workflows/StartTriggers.ps1
@@ -1,4 +1,4 @@
 ﻿param ($dataFactoryName, $resourceGroupName)
 
-$triggersADF = Get-AzDataFactoryV2Trigger -DataFactoryName dataFactoryName -ResourceGroupName $resourceGroupName
+$triggersADF = Get-AzDataFactoryV2Trigger -DataFactoryName $dataFactoryName -ResourceGroupName $resourceGroupName
 $triggersADF | ForEach-Object { Start-AzDataFactoryV2Trigger -ResourceGroupName $resourceGroupName -DataFactoryName $dataFactoryName -Name $_.name -Force }
