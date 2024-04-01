7199a6ab990f3e3f91a3b1dad0ea7e6092d2b8db
Chris <chris.dudziak@gmail.com>
2024-04-01 16:09:53 -0400

    added trigger scripts

---
+++ .github/workflows/StopTriggers.ps1
@@ -0,0 +1,6 @@
+﻿# You can write your azure powershell scripts inline here. 
+# You can also pass predefined and custom variables to this script using arguments
+
+$triggersADF = Get-AzDataFactoryV2Trigger -DataFactoryName "lens-adf-3450" -ResourceGroupName "rg-lens-prod"
+
+$triggersADF | ForEach-Object { Stop-AzDataFactoryV2Trigger -ResourceGroupName "rg-lens-prod" -DataFactoryName "lens-adf-3450" -Name $_.name -Force }
